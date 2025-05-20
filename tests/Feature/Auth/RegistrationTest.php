8f17f4b018790adadedace1df8f4c618c1a71b6f
Him <abdulrahimhim2@gmail.com>
2025-05-20 14:59:55 +0700

    fix

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -12,6 +12,7 @@ test('new users can register', function () {
     $response = Volt::test('auth.register')
         ->set('name', 'Test User')
         ->set('email', 'test@example.com')
+        ->set('username', 'testuser') // ✅ เพิ่มตรงนี้
         ->set('password', 'password')
         ->set('password_confirmation', 'password')
         ->call('register');
